3b9a685069f7d1bdae232a40b29c742cfabf51ca
Scelta <scelta21st@gmail.com>
2016-01-08 18:12:12 +0800

    optimized for common use

--- bin/genomics/soap2BuildAbudance.dev.pl
+++ bin/genomics/soap2BuildAbudance.dev.pl
@@ -25,7 +25,7 @@ $parameter ||= ",r=0,l=30,M=4,S,p=8,v=5,S,c=0.95";
 $parameter =~ s/,/ -/g;
 $parameter =~ s/=/ /g;
 
-$ab || = 123;
+$ab ||= 123;
 chomp (my $pwd=`pwd`);
 $workpath||=$pwd;
 
@@ -39,16 +39,14 @@ if($sample_single){
 	my $cmd = "$soap_path -a $sample_pair_1 -b $sample_pair_2 -D $db_index1 -D $db_index2 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log\n";
 	print STDERR "$cmd\n";
 	`$cmd`;print STDERR "PE soap finished!\n";
-	$cmd = "$soap_path -a $sample_single -D $db_index1 -D $db_index2 -o $workpath/$prefix.gene.build/$prefix.soap.single.se $parameter 2>$workpath/$prefix.gene.build/$prefix.soap.single.log";
-	 print STDERR "$cmd\n";
+	$cmd = "$soap_path -a $sample_single -D $db_index1 -D $db_index2 -o $workpath/$prefix.gene.build/$prefix.soap.single.se $parameter 2>$workpath/$prefix.gene.build/$prefix.soap.single.log\n";
+	 print STDERR "$cmd";
 	`$cmd`;print STDERR "SE soap finished!\n";
-}
-elsif($sample_pair_2){
-	my $cmd = "$soap_path -a $sample_pair_1 -b $sample_pair_2 -D $db_index1 -D $db_index2 -m 226 -x 426 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log";
-	print STDERR "$cmd\n";
+}elsif($sample_pair_2){
+	my $cmd = "$soap_path -a $sample_pair_1 -b $sample_pair_2 -D $db_index1 -D $db_index2 -m 226 -x 426 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log\n";
+	print STDERR "$cmd";
 	`$cmd`;print STDERR "PE soap finished!\n";
-	}
-elsif($sample_pair_1){
+}elsif($sample_pair_1){
 	my $cmd = "$soap_path -a $sample_pair_1 -D $db_index1 -D $db_index2 -m 226 -x 426 -o $workpath/$prefix.gene.build/$prefix.soap.pair.pe -2 $workpath/$prefix.gene.build/$prefix.soap.pair.se $parameter 2> $workpath/$prefix.gene.build/$prefix.soap.pair.log\n";
 	print STDERR "$cmd";
 	`$cmd`;print STDERR "SE soap finished!\n";
@@ -69,12 +67,12 @@ if ($ab =~ /1/){
 	print STDERR "reads abudance file built\n";
 }
 if ($ab =~ /2/){
-	$cmd = "perl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/profile/05.LostReads_ProfileAdjust/gene_Profiling.cxh.v3.all.pl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/list/760MetaHit_139HMP_368PKU_511Bac.uniq.fa.len $ins $workpath/$prefix.soap.list $workpath/$prefix.base\n";
+	my $cmd = "perl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/profile/05.LostReads_ProfileAdjust/gene_Profiling.cxh.v3.all.pl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/list/760MetaHit_139HMP_368PKU_511Bac.uniq.fa.len $ins $workpath/$prefix.soap.list $workpath/$prefix.base\n";
 	print STDERR $cmd;`$cmd`;
 	print STDERR "base abudance file built\n";
 }
 if ($ab =~ /3/){
-	$cmd = "perl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/profile/05.LostReads_ProfileAdjust/gene_Profiling.cxh.v4.all.pl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/list/760MetaHit_139HMP_368PKU_511Bac.uniq.fa.len $ins $workpath/$prefix.soap.list $workpath/$prefix.aj\n";
+	my $cmd = "perl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/profile/05.LostReads_ProfileAdjust/gene_Profiling.cxh.v4.all.pl /ifs1/ST_MD/PMO/SZC08004_MetaHIT/User/caixianghang/06.Profile/1.GeneProfile/list/760MetaHit_139HMP_368PKU_511Bac.uniq.fa.len $ins $workpath/$prefix.soap.list $workpath/$prefix.aj\n";
 	print STDERR $cmd;`$cmd`;
 	print STDERR "adjusted abudance file built\n";
 }
