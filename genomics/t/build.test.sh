b07a01c6683bb71a2ee3ec37183efb0b7c72339d
Scelta <scelta21st@gmail.com>
2016-01-12 11:17:21 +0800

    change result directory

--- genomics/t/build.test.sh
+++ genomics/t/build.test.sh
@@ -1,1 +1,1 @@
-perl ../CHAOmics_MetaGenomics_Init.pl -p test.5samples.path -i test.5samples.ins -s 123 -o test_result -c Qt=20,l=100,N=1,Qf=15,lf=35
+perl ../CHAOmics_MetaGenomics_Init.pl -p test.5samples.path -i test.5samples.ins -s 123 -o result -c Qt=20,l=100,N=1,Qf=0,lf=35
